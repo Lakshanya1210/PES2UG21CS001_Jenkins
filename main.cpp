ef70523983133a6e32266f9bab536238d4a09567
Lakshanya1210 <92517428+Lakshanya1210@users.noreply.github.com>
2024-03-08 12:49:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,33 @@
 pipeline {
-  agent any
-  stages{
-    // stage('Clone repository') {
-    // steps {
-    //     checkout([$class:'GitSCM',
-    //     branches : [[name: '*/main']],
-    //     userRemoteConfigs :[[url: 'https://github.com/Lakshanya1210/PES2UG21CS001_Jenkins.git']]])
-    //  }
-    //}
-    stage('Build'){
-      steps {
-        build 'PES2UG21CS001-1'
-        sh 'g++ main.cpp -o output'
-      }
-    }
-    stage('Test'){
-      steps{
-        sh './output'
-      }
-    }
-    stage('Deploy'){
-      steps {
-        echo 'deploy'
-      }
-    }
-  }
-  post {
-    failure {
-      error 'Pipeline failed'
-    }
-  }
+	agent any
+	stages {
+		stage ('Build') {
+			steps {
+				sh 'mvn clean install'
+				echo 'Build Stage Successful'
+			}
+		}
+		stage ('Test') {
+			steps {
+				sh 'mvn test'
+				echo 'Test Stage Successful'
+				post {
+					always {
+						junit 'target/surefire-reports/*.xml'
+					}
+				}
+			}
+		}
+		stage ('Deploy') {
+			steps {
+				sh 'mvn deploy'
+				echo 'Deployment Successful'
+			}
+		}
+	}
+	post {
+		failure {
+			echo 'Pipeline Failed'
+		}
+	}
 }
